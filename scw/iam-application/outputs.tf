fb1d86705dce31b3051fbffb6a35f1b844a88a89
Frederic Leger <frederic@webofmars.com>
2023-05-07 21:42:18 +0200

    feat(scw): add IAM application

---
+++ scw/iam-application/outputs.tf
@@ -0,0 +1,4 @@
+output "id" {
+  description = "The ID of the application"
+  value       = scaleway_iam_application.app.id
+}
---
+++ scw/iam-application/providers.tf
@@ -0,0 +1,9 @@
+terraform {
+  required_providers {
+    scaleway = {
+      source  = "scaleway/scaleway"
+      version = "~> 2.17.0"
+    }
+  }
+  required_version = "~> 1.3"
+}
