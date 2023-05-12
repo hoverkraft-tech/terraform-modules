76a55351e7b16b3a7758a896263bb106a441b1de
Frederic Leger <frederic@webofmars.com>
2023-05-12 21:05:57 +0200

    feat: add k8s ns

---
+++ k8s/k8s-namespace/providers.tf
@@ -0,0 +1,21 @@
+# NOTE: you might want to override this files with your own if you are using terragrunt
+#       and are using more module to compose your stack
+
+terraform {
+  required_version = "~> 1.3"
+  required_providers {
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.2.3"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.16.1"
+    }
+  }
+}
+
+provider "kubernetes" {
+  config_path    = var.config_path
+  config_context = var.config_context
+}
