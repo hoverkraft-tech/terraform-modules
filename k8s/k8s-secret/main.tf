24b78c143bf2a319aeea177fcbf7a893d525ce8e
Frederic Leger <frederic@webofmars.com>
2024-12-06 19:06:04 +0100

    fix(k8s): labels must comply with DNS RFC1123

--- k8s/k8s-secret/main.tf
+++ k8s/k8s-secret/main.tf
@@ -1,10 +1,11 @@
 locals {
   interpolated_tags = merge({
-    "Name"           = var.name,
-    "Customer"       = var.customer,
-    "Environment"    = var.environment,
-    "ManagedBy"      = "Terraform",
-    "LastModifiedAt" = time_static.last_update.rfc3339,
+    # NOTE: label names must comply with DNS rfc1123
+    "name"             = var.name,
+    "customer"         = var.customer,
+    "environment"      = var.environment,
+    "managed-by"       = "Terraform",
+    "last-modified-at" = time_static.last_update.rfc3339,
     },
     var.tags
   )
