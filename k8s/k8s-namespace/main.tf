9d463de864362b35054f70630e954843c0d2eebf
Frederic Leger <frederic@webofmars.com>
2025-02-28 13:38:17 +0100

    fix: comply with DNS RFC for all k8s objects

--- k8s/k8s-namespace/main.tf
+++ k8s/k8s-namespace/main.tf
@@ -1,10 +1,11 @@
 locals {
   interpolated_tags = merge({
-    "Name"           = var.name,
-    "Customer"       = var.customer,
-    "Environment"    = var.environment,
-    "ManagedBy"      = "Terraform",
-    "LastModifiedAt" = time_static.last_update.rfc3339,
+    # NOTE: label names must comply with DNS rfc1123
+    "name"             = lower(replace(var.name, "/[^a-zA-Z0-9-]/", "-")),
+    "customer"         = lower(replace(var.customer, "/[^a-zA-Z0-9-]/", "-")),
+    "environment"      = lower(replace(var.environment, "/[^a-zA-Z0-9-]/", "-")),
+    "managed-by"       = "terraform",
+    "last-modified-at" = formatdate("YYYYMMDD-hhmmss", time_static.last_update.rfc3339),
     },
     var.tags
   )
