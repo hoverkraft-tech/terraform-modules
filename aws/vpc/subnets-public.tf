47093fd739e23e502981a9f371a4bd26425f711c
Frederic Leger <frederic@webofmars.com>
2025-05-22 15:42:59 +0200

    fix(aws/vpc): configures NAT gateway to public subnet
    Updates the NAT gateway configuration to associate with the public subnet instead of the private subnet.
    
    Also, it fixes the EIP and NAT gateway tag configuration by adjusting the order of merged tags.
    It sets `map_public_ip_on_launch` to true on public subnets.

--- aws/vpc/subnets-public.tf
+++ aws/vpc/subnets-public.tf
@@ -1,8 +1,9 @@
 resource "aws_subnet" "pub_subnet" {
-  count             = length(var.availability_zones)
-  vpc_id            = aws_vpc.vpc.id
-  cidr_block        = var.public_subnets[var.availability_zones[count.index]].cidr
-  availability_zone = var.availability_zones[count.index]
+  count                   = length(var.availability_zones)
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = var.public_subnets[var.availability_zones[count.index]].cidr
+  availability_zone       = var.availability_zones[count.index]
+  map_public_ip_on_launch = true
   tags = merge(
     local.interpolated_tags,
     {
