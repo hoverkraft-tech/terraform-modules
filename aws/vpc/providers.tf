b16479d10385aa99bf6df4655d29bd64b8a0a5ab
Frederic Leger <frederic@webofmars.com>
2025-10-13 17:23:55 +0200

    chore: upgrade all tf providers

--- aws/vpc/providers.tf
+++ aws/vpc/providers.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws",
-      version = ">= 4.30.0, < 5.0.0"
+      version = ">= 4.67.0, < 5.0.0"
     }
     time = {
       source  = "hashicorp/time",
