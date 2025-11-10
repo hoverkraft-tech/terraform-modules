995df4bf6e5c8f9674181058089d226d22626223
Florian FEDERIGHI <ffederighi@beetween.com>
2025-11-10 17:31:11 +0100

    feat: switch to ovh provider for private subnet

--- ovh/pci-private-subnet/private-subnet.tf
+++ ovh/pci-private-subnet/private-subnet.tf
@@ -1,14 +1,10 @@
-resource "openstack_networking_subnet_v2" "subnet" {
-  name       = var.name
-  network_id = var.network_id
-  cidr       = var.ip.network
-  ip_version = 4
-
-  dns_nameservers = var.dns_nameservers
-
-  allocation_pool {
-    start = var.ip.start
-    end   = var.ip.end
-  }
-  tags = [for k, v in local.interpolated_tags : "${k}=${v}"]
+resource "ovh_cloud_project_network_private_subnet" "subnet" {
+  service_name = var.cloud_project_id
+  network_id   = var.network_id
+  region       = var.region
+  network      = var.network
+  start        = var.start
+  end          = var.end
+  dhcp         = var.dhcp
+  no_gateway   = var.no_gateway
 }
