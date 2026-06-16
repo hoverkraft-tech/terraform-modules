bac9a13699fb2b0a1808a76e929d4f30838886bc
Frederic Leger <frederic@webofmars.com>
2026-06-16 17:37:35 +0200

    feat(helm): add helm template support
    Signed-off-by: Frederic Leger <frederic@webofmars.com>

---
+++ k8s/helm-template/providers.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = "~> 1.3"
+  required_providers {
+    helm = {
+      source  = "hashicorp/helm",
+      version = ">= 2.17.0, < 3.0.0"
+    }
+    time = {
+      source  = "hashicorp/time",
+      version = "~> 0.9.1"
+    }
+  }
+}
+
+provider "helm" {
+  kubernetes {
+    config_paths   = var.kubeconfig_paths
+    config_context = var.kubeconfig_context
+  }
+}
